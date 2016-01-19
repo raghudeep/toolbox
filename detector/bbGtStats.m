241a93c41348091fe5d1a4c9438ea24e94b527e6
GADDE Raghudeep <gadder@mobius.enpc.fr>
2016-01-19 14:59:25 +0100

    finetuned parameters

--- detector/bbGtStats.m
+++ detector/bbGtStats.m
@@ -1,44 +1,33 @@
 function varargout = bbGtStats( varargin )
 %function CreateGTAnnotations(dataList,gtLabelDir,annotationDir,statsDir,doorLabel,windowLabel,numClasses)
 
-prms = getPrmDflt(varargin,{'dataList','','gtLabelDir','','annotationDir','',...
-       'statsDir','','clsLabels',[],'numClasses',1},1);
+prms = getPrmDflt(varargin,{'dataList','','lblDir','','annotationDir','',...
+       'statsDir','','clsLabels',[],'numClasses',1},0);
 
-if ~exist(prms.modelDir,'dir') mkdir(prms.modelDir); end
 if ~exist(prms.annotationDir,'dir') mkdir(prms.annotationDir); end
 if ~exist(prms.statsDir,'dir') mkdir(prms.statsDir); end
 
 clsParams = {}; clsStats = {};
-for i=1:prms.cls.numClasses
-    clsParams{i} = [];
-end
-
+for i=1:prms.numClasses clsParams{i} = []; end
 for i=1:length(prms.dataList)    
 		fprintf(1,'%d. %s\n',i,prms.dataList{i});
-    %Load segmentation labels for the image
-    labels = load([prms.gtLabelDir '/' prms.dataList{i} '.txt']);    
+    labels = load([prms.lblDir '/' prms.dataList{i} '.txt']);    
     numObjects = 0; objTypes = []; objParams = [];
     for j=1:prms.numClasses 
 			  if ismember(j-1,prms.clsLabels)
-            bwImage = labels == j-1; 
-            params = GetParams(bwImage);
+            bwImage = labels == j-1; params = GetParams(bwImage);
             for t=1:size(params,1)
-               objParams = [objParams; params(t,:)];
-               numObjects = numObjects + 1;
-               objTypes{numObjects} = j-1; 
-               clsParams{j} = [clsParams{j}; params(t,1:4)];
+               objParams = [objParams; params(t,:)]; numObjects = numObjects + 1;
+               objTypes{numObjects} = num2str(j-1); clsParams{j} = [clsParams{j}; params(t,1:4)];
     					 % maybe add 'other' class
             end
 				end
     end
     objs = bbGt('create',[numObjects]);
     for k=1:numObjects
-        objs(k).lbl = objTypes{k};
-        objs(k).bb = objParams(k,1:4);
-        %objs(k).occ = objParams(k,5:8);
-        %objs(k). bbv = objParams(k,9);
-        %objs(k).ign = objParams(k,10);
-        %objs(k).ang = objParams(k,11);
+        objs(k).lbl = objTypes{k}; objs(k).bb = objParams(k,1:4);
+        %objs(k).occ = objParams(k,5:8); %objs(k). bbv = objParams(k,9);
+				%objs(k).ign = objParams(k,10); %objs(k).ang = objParams(k,11);
     end
     fName = [prms.annotationDir '/' prms.dataList{i} '.txt'];
     objs = bbGt('bbSave', objs, fName);
@@ -47,8 +36,8 @@ end
 for j=1:prms.numClasses 
     stats = {}; params = clsParams{j};
 		if size(params,1)>0
-        stats.xRange = [max(0,min(params(:,1))-100) max(params(:,1))+100];
-        stats.yRange = [max(0,min(params(:,2))-100) max(params(:,2))+100];
+        stats.xRange = [max(0,min(params(:,1))-100) max(params(:,1))+200];
+        stats.yRange = [max(0,min(params(:,2))-100) max(params(:,2))+200];
         stats.hRange = [max(0,min(params(:,4))-20) max(params(:,4))+20];
         stats.wRange = [max(0,min(params(:,3))-10) max(params(:,3))+10];
         stats.modelDs = [mean(params(:,4))-10 mean(params(:,3))-10];
@@ -56,22 +45,19 @@ for j=1:prms.numClasses
         clsStats{j} = stats;
 		end
 end
-save([prms.statsDir '/stats.mat'],'clsStats');
+%save([prms.statsDir '/stats.mat'],'clsStats'); 
+stats.clsStats=clsStats; varargout{1}=stats;
 
 end
 
 function params = GetParams(bwImage)
 
 params = [];
-bwImage = bwareaopen(bwImage, 200);
-CC = bwconncomp(bwImage);
-nObjects = CC.NumObjects;
-imgL = bwlabel(bwImage);
+bwImage = bwareaopen(bwImage, 200); CC = bwconncomp(bwImage);
+nObjects = CC.NumObjects; imgL = bwlabel(bwImage);
 for i=1:1:nObjects
-    [indX,indY]=find(imgL==i);
-    obj_l=min(indY);obj_t=min(indX);
-    obj_w=max(indY)-min(indY);
-    obj_h=max(indX)-min(indX);
+    [indX,indY]=find(imgL==i); obj_l=min(indY);obj_t=min(indX);
+    obj_w=max(indY)-min(indY); obj_h=max(indX)-min(indX);
     params = [params; obj_l obj_t obj_w obj_h 0 0 0 0 0 0 0];
 end
 
--- detector/runDetect.m
+++ detector/runDetect.m
@@ -1,33 +1,37 @@
 function varargout = runDetect( varargin )
 %function DoDetections(trainDataFiles,testDataFiles,imgDir,annotationDir,statsDir,modelDir,outputDir,doorLabel,windowLabel,numClasses)
 
-prms = getPrmDflt(varargin,{'trainDataFiles','','testDataFiles','','annotationDir','',...
-       'imgDir','','statsDir','','modelDir','','clsLabels',[],'numClasses',1,'outputDir',''},1);
-opts = acfTrain();
+prms = getPrmDflt(varargin,{'trainDataFiles','','testDataFiles','','annotationDir','','lblDir','',...
+       'imgDir','','statsDir','','modelDir','','clsLabels',[],'numClasses',1,'outputDir','','clsCascCal',[]},0);
+prms.dataList = prms.trainDataFiles;
+stats = bbGtStats( prms ); opts = acfTrain();
 opts.pPyramid.smooth=.5; opts.pPyramid.pChns.pColor.smooth=0;
-opts.stride = 1;
-opts.pNms.overlap = 0.99999;
-opts.nWeak=[32 128 512 2048];
-opts.pJitter=struct('flip',1);
-opts.pBoost.pTree.fracFtrs=1/16;
-opts.posGtDir=prms.annotationDir;
-opts.posImgDir=prms.imgDir;
-stats = load([prms.statsDir '/stats.mat']);
+opts.stride = 1; opts.pNms.overlap = 0.99999;
+opts.nWeak=[32 128 512 2048]; opts.pJitter=struct('flip',1);
+opts.pBoost.pTree.fracFtrs=1/16; opts.posGtDir=prms.annotationDir;
+%stats = load([prms.statsDir '/stats.mat']);
+tempImgDir = tempname; mkdir(tempImgDir);
+for i=1:length(prms.trainDataFiles)
+	copyfile([prms.imgDir '/' prms.trainDataFiles{i} '.png'], tempImgDir);
+end
+opts.posImgDir=tempImgDir;
 
 if ~exist(prms.modelDir,'dir') mkdir(prms.modelDir); end
 if ~exist(prms.modelDir,'dir') mkdir(prms.outputDir); end
 
-for j=1:numClasses 
+for j=1:prms.numClasses 
 	  if ismember(j-1,prms.clsLabels)
         opts.name=[prms.modelDir '/' num2str(j-1)];
         opts.modelDs=stats.clsStats{j}.modelDs; opts.modelDsPad=stats.clsStats{j}.modelDsPad;
-        pLoad={'lbls',{'door'},'ilbls',{'doors'}}; %% check
-        opts.pLoad = [pLoad 'yRng',stats.clsStats(j).yRange, 'xRng',stats.clsStats(j).xRange,...
-				              'hRng',stats.clsStats(j).hRange, 'wRng',stats.clsStats(j).wRange ];
+        pLoad={'lbls',{num2str(j-1)},'ilbls',{[num2str(j-1) 's']}}; % check
+        opts.pLoad = [pLoad 'yRng',stats.clsStats{j}.yRange, 'xRng',stats.clsStats{j}.xRange,...
+				              'hRng',stats.clsStats{j}.hRange, 'wRng',stats.clsStats{j}.wRange ];opts
         detector = acfTrain(opts);
-        detector = acfModify(detector,'cascThr',-1,'cascCal',0.1,'stride',1);%% check
-        for t=1:length(testDataFiles)
-            I = imread([prms.imgDir testDataFiles{t} '.png']);
+        detector = acfModify(detector,'cascThr',-1,'cascCal',prms.clsCascCal(j),'stride',1);
+				leftRange=stats.clsStats{j}.xRange; topRange=stats.clsStats{j}.yRange;
+				widthRange=stats.clsStats{j}.wRange; heightRange=stats.clsStats{j}.hRange;
+        for t=1:length(prms.testDataFiles)
+            I = imread([prms.imgDir prms.testDataFiles{t} '.png']);
             bbs = acfDetect(I,detector); bbs_final = [];
             for s=1:size(bbs,1)
                 if (bbs(s,1) > leftRange(1)) && (bbs(s,1) < leftRange(2))
@@ -46,11 +50,10 @@ for j=1:numClasses
                     featureImage = featureImage + GetDetectionMask(imgSize,bbs_final(k,:),bbs_final(k,5));
                 end
             end
-            featureImage = featureImage';
-            features = featureImage(:);
+            featureImage = featureImage'; features = featureImage(:);
             %dlmwrite(strcat(outputDir,'/',img_name,'.doorfeatures.txt'),doorFeatures,'delimiter',' ');
-            fid=fopen([prms.outputDir '/' testDataFiles{t} '.' num2str(j-1) '.bin'],'wb');
-            fwrite(fid,doorFeatures,'float32'); fclose(fid);
+            fid=fopen([prms.outputDir '/' prms.testDataFiles{t} '.' num2str(j-1) '.bin'],'wb');
+            fwrite(fid,features,'float32'); fclose(fid);
         end
 		end
 end
